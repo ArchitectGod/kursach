7485a04c676dd2b32b897fe8ec095b33b03815fd
Architect <dudyakina@gmail.com>
2025-09-29 18:53:20 +0700

    Добавил функцию установки флага на клетку

--- kursach.cpp
+++ kursach.cpp
@@ -4,13 +4,48 @@ int main()
 {
     printf("Привет, Мир!");
 
-    typedef struct {
-        char name[50];       // имя игрока (строка, максимум 49 символов + '\0')
-        int timeSpent;       // время игры в секундах
-        int openedCells;     // количество открытых клеток
-        int mistakes;        // число ошибок (например, неверные флаги или взрывы)
-        int bestTime;        // лучшая попытка (время в секундах, меньше — лучше)
-    } Player;
 
+    
 }
+
+typedef struct {
+    char name[50];       // имя игрока (строка, максимум 49 символов + '\0')
+    int timeSpent;       // время игры в секундах
+    int openedCells;     // количество открытых клеток
+    int mistakes;        // число ошибок (например, неверные флаги или взрывы)
+    int bestTime;        // лучшая попытка (время в секундах, меньше — лучше)
+} Player;
+
+int toggleFlag(Board board, int x, int y, Player player)
+{
+    int index = y * board.width + x; // Вычисление индекса клетки в массиве
+    if (x < 0 || x >= board.width || y < 0 || y >= board.height) // Проверка, корректны ли координаты
+    {
+        return -1; // Ошибка! координаты вне поля
+    }
+    Cell cell = board.cells[index]; // Получаем клетку
+    if (cell.isOpen == 1) // Если клетка открыта
+    {
+        return 0; // Флаг на открытую клетку нельзя поставить
+    }
+    if (cell.isFlag == 0) // Если на клетке нет флага
+    {
+        board.cells[index].isFlag = 1; // Устанавливаем флаг
+        if (cell.isBomb == 0) // Если на клетке нет бомбы
+        {
+            player.mistakes = player.mistakes + 1; // +ошибка из-за неправильного отмеченного флага
+        }
+    }
+    else // Если на клетке есть флаг
+    {
+        board.cells[index].isFlag = 0; // Снимаем флаг
+        if (cell.isBomb == 0)
+        {
+            player.mistakes = player.mistakes - 1; // Убираем ошибку, если флаг был неверный
+        }
+    }
+    return 0; // Всё прошло успешно, игра продолжается
+}
+
+
 
